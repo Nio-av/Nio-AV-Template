690e2a84ae1e709b9de743ace7691e09cb85dea2
Nio-av <public@nio-av.de>
2015-03-31 01:49:24 +0200

    reduced number of Classes for posts Link
    only one class for next and previous link

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ add_filter('next_posts_link_attributes', 'posts_link_attributes');
 add_filter('previous_posts_link_attributes', 'posts_link_attributes');
 //
 function posts_link_attributes() {
-    return 'class="postnav window"';
+    return 'class="postnav"';
 }
 
 
