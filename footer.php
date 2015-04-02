5deff497a171374c4caea9866deae1aad1b726ea
Nio-av <public@nio-av.de>
2015-04-02 12:56:04 +0200

    increased padding for Social Media Buttons

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
                     <a href="//twitter.com/Nio_vs" target="_blank"><img src="<?php echo(get_template_directory_uri()); ?>/PlugIns/SocialMediaIcons/Twitter.svg" alt="Twitter"></a>
                     <a href="//github.com/Nio-av" target="_blank"><img src="<?php echo(get_template_directory_uri()); ?>/PlugIns/SocialMediaIcons/GitHub.svg" alt="GitHub"></a>
                     <a href="//vimeo.com/niovs" target="_blank"><img src="<?php echo(get_template_directory_uri()); ?>/PlugIns/SocialMediaIcons/Vimeo.svg" alt="Vimeo"> </a>
-                    <a href="//nio-av.de/kalender" target="_blank"><button type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-calendar "></span></button></a>
+                    <a href="//nio-av.de/kalender" target="_blank"><button type="button" class="btn btn-default btn-lg bootstrap-button"><span class="glyphicon glyphicon-calendar "></span></button></a>
                 </div>
 
                 <nav id="footernav">
