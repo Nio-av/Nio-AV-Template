e706848b2f2b47e6bfad58ea63da31a4c08319c1
Nico <Nio-av@users.noreply.github.com>
2019-04-09 00:16:47 +0200

    make header sticky

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" defer></script>
+    <script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" defer></script>
     <script src="<?php echo(get_template_directory_uri()); ?>/PlugIns/fitty/fitty.js"></script>
 
 
@@ -34,7 +34,7 @@
 
 </head>
 <body>
-<header>
+<header class="grid centeredContent">
 
     <h1 id="posttitel">
         <div class="fitty verticalCenter">
@@ -45,16 +45,18 @@
     <div class="desktoponly" id="branding">
         <div class="verticalCenter">
             <div class="centerChilds">
-            <a href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?>
-            </a>
-            <?php
-            if (get_bloginfo('description') != false) {
-                echo '<div id="pageDescription">';
-                bloginfo('description');
-                echo '</div>';
-            }
-            ?>
+                <a href="<?php echo home_url(); ?>">
+                    <?php bloginfo('name'); ?>
+
+                </a>
+                <?php
+                if (get_bloginfo('description') != false) {
+                    echo '<div id="pageDescription">';
+                    bloginfo('description');
+                    echo '</div>';
+                }
+                ?>
+
             </div>
         </div>
     </div>
@@ -68,31 +70,30 @@
         });
     </script>
 
-
-    <nav class="navbar navbar-expand-md navbar-light" role="navigation">
-        <div class="container">
-
-            <a class="navbar-brand mobileonly" href="#"><?php bloginfo('name'); ?></a>
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-
-
-            <?php /* Primary navigation */
-            wp_nav_menu( array(
-                'theme_location'  => 'header-menu',
-                'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
-                'container'       => 'div',
-                'container_class' => 'collapse navbar-collapse',
-                'container_id'    => 'bs-example-navbar-collapse-1',
-                'menu_class'      => 'navbar-nav mr-auto',
-                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                'walker'          => new WP_Bootstrap_Navwalker(),
-            ) );
-            ?>
-        </div>
-    </nav>
-
 </header>
+
+<nav class="navbar navbar-expand-md navbar-light sticky-top centeredContent blur" role="navigation">
+    <div class="container">
+
+        <a class="navbar-brand mobileonly" href="#"><?php bloginfo('name'); ?></a>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+
+        <?php /* Primary navigation */
+        wp_nav_menu( array(
+            'theme_location'  => 'header-menu',
+            'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+            'container'       => 'div',
+            'container_class' => 'collapse navbar-collapse',
+            'container_id'    => 'bs-example-navbar-collapse-1',
+            'menu_class'      => 'navbar-nav mr-auto',
+            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'          => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+    </div>
+</nav>
