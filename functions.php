245c0526e295f4fb5a6bafb4f2308e207c30992c
Nico <Nio-av@users.noreply.github.com>
2019-04-16 21:27:11 +0200

    include social media icons via before
    http://krasimirtsonev.com/blog/article/CSS-before-and-after-pseudo-elements-in-practice
    
    https://stackoverflow.com/questions/4912765/hide-element-but-show-css-generated-content
    
    https://stackoverflow.com/questions/8977957/can-i-change-the-height-of-an-image-in-css-before-after-pseudo-elements

--- functions.php
+++ functions.php
@@ -6,7 +6,10 @@
 add_action( 'init', 'register_my_menus' );
 function register_my_menus() {
 	register_nav_menus(array('header-menu' => __('Header Menu'),
-							'footer-menu' => __('Footer Menu')));
+							'footerMenu' => __('Footer Menu'),
+                            'socialMenu' => __('Social Menu'),
+        )
+    );
 }
 
 
--- page.php
+++ page.php
@@ -21,4 +21,4 @@
 
 
 
-<?php //get_footer(); ?>
+<?php get_footer(); ?>
