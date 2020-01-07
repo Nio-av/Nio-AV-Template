9b4ff6fd5ccd9a1563a2137a68b32b9e7f7f525e
Nico <Nio-av@users.noreply.github.com>
2020-01-07 20:19:32 +0100

    improve layout of categorys

--- single.php
+++ single.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<div id="postinformation" class="center">
+<div id="postinformation" class="centeredContent">
     <nav>
         <?php
             $categories = get_the_category();
-            $separator = ' // ';
+            $separator = " // ";
             if($categories){
                 foreach($categories as $category) {
                     $output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>' . $separator;
